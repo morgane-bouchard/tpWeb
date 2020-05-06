0c145e4b1a0e9012ae28bc247f56ecf4af0aa0d8
mobouchard <morgane.bouchard@etudiant.univ-rennes1.fr>
2020-05-06 11:33:15 +0100

    La vue

--- model.js
+++ model.js
@@ -7,25 +7,25 @@ function Dessin(){
     this.removeForme = function(id) { this.formes.splice(id,1); }.bind(this);
 }
 
-function Forme(couleur, epaisseur) {
+function Shape(couleur, epaisseur) {
     this.couleur = couleur;
     this.epaisseur =epaisseur;
 }
 
-function Rectangle(x, y, largeur, hauteur) {
-    Forme.call(this,couleur,epaisseur);
-    this.x1 = x1;
-    this.y1 = y1;
+function Rectangle(x, y, hauteur, largeur) {
+    Shape.call(this,couleur,epaisseur);
+    this.x = x;
+    this.y = y;
     this.largeur = largeur;
     this.hauteur = hauteur;
 }
-Rectangle.prototype = new Forme();
+Rectangle.test = new Forme();
 
 function Ligne(x1, y1, x2, y2) {
-    Forme.call(this,color,thickness);
+    Shape.call(this,couleur,thickness);
     this.x1 = x1;
     this.y1 = y1;
     this.x2 = x2;
     this.y2 = y2;
 }
-Ligne.prototype = new Forme();
+Ligne.test = new Shape();
--- view.js
+++ view.js
@@ -1,2 +1,25 @@
 
 // Implémenter ici les fonctions paint à ajouter dans chacune des classes du modèle.
+Rectangle.prototype.paint = function(ctx) {
+    Shape.test.paint.call(this, ctx);
+    ctx.rect(this.x, this.y, this.hauteur, this.largeur);
+    ctx.stroke();
+};
+
+Ligne.prototype.paint = function(ctx) {
+    Shape.test.paint.call(this, ctx);
+    ctx.beginPath();
+    ctx.moveTo(this.x1, this.y1);
+    ctx.lineTo(this.x2, this.y2);
+    ctx.stroke();
+};
+
+
+Dessin.prototype.paint = function(ctx) {
+    console.log(this.formes);
+    ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+    ctx.fillRect(0, 0, canvas.width, canvas.height);
+    this.formes.forEach(function (element) {
+        element.paint(ctx);
+    });
+};
