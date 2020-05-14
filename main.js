573875f94ddf22579c294673fa18ae36372f32fc
mobouchard <morgane.bouchard@etudiant.univ-rennes1.fr>
2020-05-14 22:58:08 +0100

    Le contrôleur qu11

--- main.js
+++ main.js
@@ -6,9 +6,9 @@ canvas.width=800
 canvas.height=600
 
 // Code temporaire pour tester le DnD
-new DnD(canvas);
-ctx.fillStyle = '#F0F0F0'; // set canvas' background color
-ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
+//new DnD(canvas);
+//ctx.fillStyle = '#F0F0F0'; // set canvas' background color
+//ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 /////
 
 // Code temporaire pour tester l'affiche de la vue
@@ -20,7 +20,7 @@ ctx.fillRect(0, 0, canvas.width, canvas.height);  // now fill the canvas
 ////
 
 // Code final à utiliser pour manipuler Pencil.
-//var drawing = new Drawing();
-//var pencil = new Pencil(ctx, drawing, canvas);
-//drawing.paint(ctx, canvas);
+var drawing = new Drawing();
+var pencil = new Pencil(ctx, drawing, canvas);
+drawing.paint(ctx, canvas);
 
